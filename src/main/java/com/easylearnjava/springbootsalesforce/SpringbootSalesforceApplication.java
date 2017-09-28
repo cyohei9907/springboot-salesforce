84d3c68a6dd67689e3bc0e1f84bcbe57a8419c02
Easy Learn Java <easylearnjava4u@gmail.com>
2017-09-28 00:38:16 -0500

    Test

--- src/main/java/com/easylearnjava/springbootsalesforce/SpringbootSalesforceApplication.java
+++ src/main/java/com/easylearnjava/springbootsalesforce/SpringbootSalesforceApplication.java
@@ -1,30 +1,15 @@
 package com.easylearnjava.springbootsalesforce;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.easylearnjava.springbootsalesforce.service.Force;
-
-import java.util.List;
 
 @SpringBootApplication
 @RestController
 @EnableOAuth2Sso
 public class SpringbootSalesforceApplication {
 	
-	@Autowired
-    private Force force;
-
-    @RequestMapping("/accounts")
-    public List<Force.Account> accounts(OAuth2Authentication principal) {
-        return force.accounts(principal);
-    }
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootSalesforceApplication.class, args);
 	}
--- src/main/java/com/easylearnjava/springbootsalesforce/model/BillingAddress.java
+++ src/main/java/com/easylearnjava/springbootsalesforce/model/BillingAddress.java
@@ -1,5 +1,8 @@
 package com.easylearnjava.springbootsalesforce.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BillingAddress {
 	
 	private String city;
